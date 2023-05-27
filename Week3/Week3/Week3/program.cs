0f66c37b218a899fb81d8775668ae5cb5352acb1
Rangathan <121978255+Rangathan@users.noreply.github.com>
2023-05-27 09:29:17 -0700

    a

---
+++ Week3/Week3/Week3/program.cs
@@ -0,0 +1,28 @@
+﻿using ClntSrvrWk1.Lessons.Week3.Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week3
+{
+    internal class program
+    {
+
+
+        static int[] numbers = new int[] { 17, 23, 47 };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program was called with");
+
+            var duck1 = new Duck(12, "Tony");
+            Console.WriteLine(duck1.GetInfo());
+
+            var rhDuck2 = new RedheadDuck(15, "Phil", "light");
+            Console.WriteLine(rhDuck2.GetInfo());
+
+            var test = new InheritanceTest();
+            test.Test1();
+        }
+    }   
+}

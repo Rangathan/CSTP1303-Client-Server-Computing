68c176cb7ade4e0533f95ad840732d770806e030
Rangathan <121978255+Rangathan@users.noreply.github.com>
2023-06-03 10:26:19 -0700

    lab4

---
+++ lab4/MyStack.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace ClntSrvrWk1.Lessons.Week4.ObjectType
+{
+    public class MyStack
+    {
+       
+        public MyStack() { }    
+        public MyStack(object[] items)
+        {
+            this.items = items;
+        }
+
+        public MyStack(int size)
+        {
+                if (size <= 0)
+                    throw new ArgumentException("Stack size must be greater than zero.");
+
+                items = new object[size];
+            
+        }
+
+        private const int Size = 5;
+        private int position;
+
+        private object[] items = new object[Size];
+
+        public void Push(object obj)
+        {
+            if (position >= items.Length)
+                throw new InvalidOperationException("Stack is full.");
+            items[position++] = obj;
+
+        }
+
+        public object Pop()
+        {
+            if (position == 0)
+                throw new InvalidOperationException("Stack is empty.");
+
+            return items[--position];
+        }
+
+        public object Peek()
+        {
+            if (position == 0)
+                throw new InvalidOperationException("Stack is empty.");
+
+            return items[position - 1];
+        }
+    }
+}
---
+++ lab4/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClntSrvrWk1.Lessons.Week4.ObjectType;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClntSrvrWk1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var stackTest = new TestStack();
+            stackTest.Test1();
+
+            var enumTest = new TestEnum();
+            enumTest.Test1();
+        }
+    }
+}
